7186810182d204279f982dc095ffb76614f64a12
Vishal <dubeyvishal714@gmail.com>
2025-12-08 23:48:32 +0530

    initial commit

---
+++ src/server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import dotenv from "dotenv";
+
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Weather Explorer API Running");
+});
+
+app.use("/weather", weatherRouter);
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500).json({ error: err.message });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
